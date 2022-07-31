07df61d5b2c0d1faea1e2b6eb99d9a85a065d5bc
WarotAsawa <warot.asawa@gmail.com>
2022-07-31 15:20:01 +0700

    Edit cost and add price model

--- converter.py
+++ converter.py
@@ -1,4 +1,5 @@
 import csv
+import re
 import sys
 import math
 from datetime import datetime
@@ -22,11 +23,13 @@ def GetLowestInstancePrice(input, ec2Cost, ec2Spec):
     cpu = 0
     ghz = 0
     memory = 0
+    priceModel = 'on-demand'
     os = "Linux"
     if input['core']    != "": cpu = int(input['core'])
     if input['ghz']     != "": ghz = float(input['ghz'])
     if input['memory']  != "": memory = float(input['memory'])
     if input['os']  != "": os = input['os']
+    if input['price-model'] != '': priceModel = input['price-model'] 
 
     print(input["Source Name"] + " : " + str(cpu) + " cores, " + str(ghz) + " GHz, " + str(memory) + " GB mem, " + os + " OS")
 
@@ -34,18 +37,18 @@ def GetLowestInstancePrice(input, ec2Cost, ec2Spec):
         #print(ec2Type)
         spec = GetInstanceTypeSpecObj(ec2Type["API Name"], ec2Spec)
         #Logic is here
-        priceModel = os + "-on-demand"
+        model = os + "-" + priceModel
         if spec['Clock Speed(GHz)'] =='unknown' and ghz !=0 : continue
         if spec['Clock Speed(GHz)'] !='unknown': 
             if ghz > float(spec['Clock Speed(GHz)']): continue
         if cpu > int(spec['vCPUs']): continue
         if memory > float(spec['Memory']): continue
-        if ec2Type[priceModel] == 'unavailable': continue
-        if float(ec2Type[priceModel]) < minCost:
-            minCost = float(ec2Type[priceModel])
+        if ec2Type[model] == 'unavailable': continue
+        if float(ec2Type[model]) < minCost:
+            minCost = float(ec2Type[model])
             input['Instance Type'] = ec2Type["API Name"]
             input['OS'] = os
-            input['Pricing Model'] = "on-demand"
+            input['Pricing Model'] = priceModel
             input['vCPUs'] = int(spec['vCPUs'])
             input['Memory'] = float(spec['Memory'])
             input['Hourly Pricing'] = minCost
@@ -70,21 +73,35 @@ def main(argv):
         result = GetLowestInstancePrice(input, ec2Cost, ec2Spec)
         #print(result)
         resultList.append(result)
+    
     #Prepare to write file
     now = datetime.now()
     date = str(now.year)+'-'+str(now.month)+'-'+str(now.day)+'-'+str(now.hour)+'-'+str(now.minute)+'-'+str(now.second)
     outFileName = fileName[0]+'-result-'+date+'.csv'
+
+    #Write File
+    sumCPU = 0;
+    sumMem =0;
+    sumHourly = 0
+    print("Writing file to : " + outFileName)
+    for result in resultList:
+        print(result["Source Name"] + " : Type : " + result["Instance Type"] + " " + str(result['vCPUs']) + " cores, " + str(result['Memory']) + " GB mem, " + str(result['Hourly Pricing']) + " $/hrs" + str(result['Hourly Pricing']*730) + " $/Month")
+        sumCPU = sumCPU + result['vCPUs']
+        sumMem = sumMem + result['Memory']
+        sumHourly = sumHourly + result['Hourly Pricing']
+    sumRow = {}
+    sumRow['Source Name'] = "Total"
+    sumRow['vCPUs'] = sumCPU
+    sumRow['Memory'] = sumMem
+    sumRow['Hourly Pricing'] = sumHourly
+    sumRow['Monthly Pricing'] = sumHourly * 730;
+
     with open(outFileName, 'w') as csvfile:
         writer = csv.writer(csvfile)
         writer.writerow(resultList[0].keys())
         for result in resultList:
             writer.writerow(result[index] for index in result.keys())
-
-    #Sucess Write File
-    print("Success write file to : " + outFileName)
-    for result in resultList:
-        print(result["Source Name"] + " : Type : " + result["Instance Type"] + " " + str(result['vCPUs']) + " cores, " + str(result['Memory']) + " GB mem, " + str(result['Hourly Pricing']) + " $/hrs")
-
+        writer.writerow(sumRow[index] for index in sumRow.keys())
 
 if __name__ == "__main__":
    main(sys.argv[1:])
