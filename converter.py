434e26aa3926494104bef5db304c0b97d2556a22
WarotAsawa <warot.asawa@gmail.com>
2022-09-03 17:45:22 +0700

    Add UpdateEC2Price

--- converter.py
+++ converter.py
@@ -1,4 +1,5 @@
 import csv
+import re
 import sys
 import math
 from datetime import datetime
@@ -14,40 +15,54 @@ def ImportDictFromCSV(filename):
 
 def GetInstanceTypeSpecObj(ec2Type, ec2Spec):
     for item in ec2Spec:
-        if item['API Name'] == ec2Type:
+        if item['instanceType'] == ec2Type:
             return item
     
-def GetLowestInstancePrice(input, ec2Cost, ec2Spec):
+def GetLowestInstancePrice(input, ec2Cost, ec2Spec, options):
     minCost = 999999.9
     cpu = 0
     ghz = 0
     memory = 0
+    priceModel = 'on-demand'
     os = "Linux"
     if input['core']    != "": cpu = int(input['core'])
     if input['ghz']     != "": ghz = float(input['ghz'])
     if input['memory']  != "": memory = float(input['memory'])
     if input['os']  != "": os = input['os']
+    if input['price-model'] != '': priceModel = input['price-model'] 
 
     print(input["Source Name"] + " : " + str(cpu) + " cores, " + str(ghz) + " GHz, " + str(memory) + " GB mem, " + os + " OS")
-
+    #Set Options
+    noGrav = False
+    includePrev = False
+    for opt in options:
+        if "no-grav" in opt: noGrav = True
+        if "include-prev" in opt: includePrev = True
     for ec2Type in ec2Cost:
         #print(ec2Type)
         spec = GetInstanceTypeSpecObj(ec2Type["API Name"], ec2Spec)
+        try: specGHz = float(spec['clockSpeed'].split(' ')[0])
+        except: specGHz = 0.1
+        specCPU = float(spec['vcpu'].split(' ')[0])
+        specMem = float(spec['memory'].split(' ')[0])
         #Logic is here
-        priceModel = os + "-on-demand"
-        if spec['Clock Speed(GHz)'] =='unknown' and ghz !=0 : continue
-        if spec['Clock Speed(GHz)'] !='unknown': 
-            if ghz > float(spec['Clock Speed(GHz)']): continue
-        if cpu > int(spec['vCPUs']): continue
-        if memory > float(spec['Memory']): continue
-        if ec2Type[priceModel] == 'unavailable': continue
-        if float(ec2Type[priceModel]) < minCost:
-            minCost = float(ec2Type[priceModel])
+        model = os + "-" + priceModel
+        if noGrav and spec['instanceType'][2]=='g': continue
+        if noGrav and spec['instanceType'][0]=='a': continue
+        if includePrev == False and spec['currentGeneration'] == "No": continue 
+        if spec['clockSpeed'] =='unknown' and ghz !=0 : continue
+        if spec['clockSpeed'] !='unknown': 
+            if ghz > specGHz: continue
+        if cpu > specCPU: continue
+        if memory > specMem: continue
+        if ec2Type[model] == 'N/A' or ec2Type[model] == '': continue
+        if float(ec2Type[model]) < minCost:
+            minCost = float(ec2Type[model])
             input['Instance Type'] = ec2Type["API Name"]
             input['OS'] = os
-            input['Pricing Model'] = "on-demand"
-            input['vCPUs'] = int(spec['vCPUs'])
-            input['Memory'] = float(spec['Memory'])
+            input['Pricing Model'] = priceModel
+            input['vCPUs'] = int(specCPU)
+            input['Memory'] = float(specMem)
             input['Hourly Pricing'] = minCost
             input['Monthly Pricing'] = minCost * 730
             #print(ec2Type["API Name"])
@@ -58,6 +73,7 @@ def GetLowestInstancePrice(input, ec2Cost, ec2Spec):
 def main(argv):
     #print(argv)
     #Load File
+    options = argv[1:]
     ec2Cost = ImportDictFromCSV('ec2-cost.csv')
     ec2Spec = ImportDictFromCSV('ec2-spec.csv')
     inputList = ImportDictFromCSV(argv[0])
@@ -67,24 +83,38 @@ def main(argv):
     #Get EC2 instance for all VMs
     resultList = []
     for input in inputList:
-        result = GetLowestInstancePrice(input, ec2Cost, ec2Spec)
+        result = GetLowestInstancePrice(input, ec2Cost, ec2Spec, options)
         #print(result)
         resultList.append(result)
+    
     #Prepare to write file
     now = datetime.now()
     date = str(now.year)+'-'+str(now.month)+'-'+str(now.day)+'-'+str(now.hour)+'-'+str(now.minute)+'-'+str(now.second)
     outFileName = fileName[0]+'-result-'+date+'.csv'
+
+    #Write File
+    sumCPU = 0;
+    sumMem =0;
+    sumHourly = 0
+    print("Writing file to : " + outFileName)
+    for result in resultList:
+        print(result["Source Name"] + " : Type : " + result["Instance Type"] + " " + str(result['vCPUs']) + " cores, " + str(result['Memory']) + " GB mem, " + str(result['Hourly Pricing']) + " $/hrs" + str(result['Hourly Pricing']*730) + " $/Month")
+        sumCPU = sumCPU + result['vCPUs']
+        sumMem = sumMem + result['Memory']
+        sumHourly = sumHourly + result['Hourly Pricing']
+    sumRow = {}
+    sumRow['Source Name'] = "Total"
+    sumRow['vCPUs'] = sumCPU
+    sumRow['Memory'] = sumMem
+    sumRow['Hourly Pricing'] = sumHourly
+    sumRow['Monthly Pricing'] = sumHourly * 730;
+
     with open(outFileName, 'w') as csvfile:
         writer = csv.writer(csvfile)
         writer.writerow(resultList[0].keys())
         for result in resultList:
             writer.writerow(result[index] for index in result.keys())
-
-    #Sucess Write File
-    print("Success write file to : " + outFileName)
-    for result in resultList:
-        print(result["Source Name"] + " : Type : " + result["Instance Type"] + " " + str(result['vCPUs']) + " cores, " + str(result['Memory']) + " GB mem, " + str(result['Hourly Pricing']) + " $/hrs")
-
+        writer.writerow(sumRow[index] for index in sumRow.keys())
 
 if __name__ == "__main__":
    main(sys.argv[1:])
---
+++ updateEC2Price.py
@@ -0,0 +1,170 @@
+import boto3
+import json, csv
+import os
+import pandas as pd
+from pkg_resources import resource_filename
+from datetime import datetime
+
+# Set FIlter tp Singapore Only
+FLT = '[{{"Field": "tenancy", "Value": "shared", "Type": "TERM_MATCH"}},'\
+      '{{"Field": "preInstalledSw", "Value": "NA", "Type": "TERM_MATCH"}},'\
+      '{{"Field": "location", "Value": "{r}", "Type": "TERM_MATCH"}},'\
+      '{{"Field": "capacitystatus", "Value": "Used", "Type": "TERM_MATCH"}}]'
+
+def translate_platform_name(operating_system, preinstalled_software):
+    os = {
+        "Linux": "Linux",
+        "RHEL": "RHEL",
+        "Red Hat Enterprise Linux with HA": "RHEL HA",
+        "SUSE": "SLES",
+        "Windows": "Windows",
+        "Linux/UNIX": "Linux",
+        "Red Hat Enterprise Linux": "RHEL",
+        "SUSE Linux": "SLES",
+    }
+    software = {
+        "":"",
+        "NA": "",
+        "SQL Std": " SQL Std",
+        "SQL Web": " SQL Web",
+        "SQL Ent": " SQL Ent",
+    }
+    return os[operating_system] + software[preinstalled_software]
+
+# Get current AWS price for an on-demand instance
+def get_price(region):
+    f = FLT.format(r=region)
+    hourPerMonth = 730
+    #Set required spec for ec2
+    specRow = ["instanceType","enhancedNetworkingSupported","intelTurboAvailable","memory","dedicatedEbsThroughput","vcpu","classicnetworkingsupport","storage","instanceFamily","intelAvx2Available","physicalProcessor","clockSpeed","ecu","networkPerformance","vpcnetworkingsupport","tenancy","usagetype","normalizationSizeFactor","intelAvxAvailable","processorFeatures","licenseModel","currentGeneration","preInstalledSw","processorArchitecture"]
+    #Temp list space for output
+    specMatrix = []
+    priceMatrix = {}
+    nextToken = "INIT"
+    totalPage = 0;
+    totalPrice = 0;
+    #Backup Old File
+    now = datetime.now()
+    date = str(now.year)+'-'+str(now.month)+'-'+str(now.day)+'-'+str(now.hour)+'-'+str(now.minute)+'-'+str(now.second)
+    try: os.rename("ec2-spec.csv", "ec2-spec-bkk-"+date+".csv")
+    except: print("ec2-spec.csv Not Found")
+    try: os.rename("ec2-cost.csv", "ec2-cost-bkk-"+date+".csv")
+    except: print("ec2-cost.csv Not Found")
+    while nextToken != "END":
+        #If INIT then send without token, else sent with token
+        if nextToken == "INIT": data = client.get_products(ServiceCode='AmazonEC2', Filters=json.loads(f));
+        else: data = client.get_products(ServiceCode='AmazonEC2', Filters=json.loads(f),NextToken=nextToken);
+
+        #If no more Token then END the loop
+        if "NextToken" in data: nextToken = data["NextToken"]
+        else: nextToken = "END"
+        totalPage += 1;
+        totalPrice += len(data['PriceList']);
+        print("Page: " + str(totalPage) + " with " + str(len(data['PriceList'])) + " records")
+        #Get all returned PriceList
+        for i in range(0,len(data['PriceList'])-1):
+            currentData = json.loads(data['PriceList'][i])
+            productAttributes = currentData["product"]["attributes"]
+            instanceType = productAttributes["instanceType"]
+            #Update Instance Spec if found new instance
+            if (instanceType in priceMatrix) == False:
+                priceMatrix[instanceType] = {}
+                priceMatrix[instanceType]["API Name"] = instanceType
+                newInstanceSpec = []
+                for attr in specRow:
+                    if attr in productAttributes:
+                        newInstanceSpec.append(productAttributes[attr])
+                    else: newInstanceSpec.append('')
+                specMatrix.append(newInstanceSpec)
+
+            #Add on-demand and all 12 RI Pricing
+            osName = productAttributes['operatingSystem']
+            sw = productAttributes["preInstalledSw"]
+            ossw = translate_platform_name(osName,sw)
+            #Get on Demand Price
+            od = currentData['terms']['OnDemand']
+            id1 = list(od)[0]
+            id2 = list(od[id1]['priceDimensions'])[0]
+            hourlyPrice = od[id1]['priceDimensions'][id2]['pricePerUnit']['USD']
+            priceMatrix[instanceType][ossw+"-on-demand"] = float(hourlyPrice)
+
+            #Get Reserved Pricing ! Some instance has no reserved offerring at all
+            if 'Reserved' in currentData['terms']: rsSet = currentData['terms']['Reserved']
+            else: continue;
+            rsSetList = list(rsSet)
+            for rs in rsSetList:
+            
+                #Get rs type (1,3 years), convertible or not, upfront type
+                rs = rsSet[rs]
+                termAttributes = rs["termAttributes"]
+                rsType = ""
+                #Set Convertible if is "convertible" then RIC or else if is "standard" then RI
+                if 'convertible' in termAttributes['OfferingClass']: rsType = rsType + "-RIC"
+                else: rsType = rsType + "-RI"
+                #Set Year
+                year = 1
+                if '3' in termAttributes['LeaseContractLength']: rsType = rsType + "-3Y"; year=3
+                else: rsType = rsType + "-1Y"
+                #Set Upfront type
+                if 'All' in  termAttributes['PurchaseOption']: rsType = rsType + "-AUF"; 
+                elif 'Partial' in termAttributes['PurchaseOption']: rsType = rsType + "-PUF"
+                else: rsType = rsType + "-NUF"
+
+                #Get Upfront and Hourly Cost
+                offerList = list(rs["priceDimensions"])
+                price = 0
+                for offer in offerList:
+                    offer = rs["priceDimensions"][offer]
+                    offerPrice = offer["pricePerUnit"]["USD"]
+                    #If hourly, it means hourly, other wise is upfront price
+                    if offer["unit"] == "Hrs": price += float(offerPrice)
+                    else: price += float(offerPrice) / year / 365 / 24
+
+                #Set Price
+                plan = ossw + rsType
+                if price <= 0: priceMatrix[instanceType][plan] = "N/A"
+                else: priceMatrix[instanceType][plan] = price
+    print("Total Records: " + str(totalPrice))
+    #Prepare to write new spec file
+    
+    outFileName = 'ec2-spec.csv'
+    with open(outFileName, 'w') as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerow(specRow)
+        for spec in specMatrix:
+            writer.writerow(spec)
+    #Write price file to json and convert to csv
+    # Writing to sample.json
+    priceMatrixStr = json.dumps(priceMatrix, indent=2)
+    with open("temp-price.json", "w") as outfile:
+        outfile.write(priceMatrixStr)
+    #use pandas to convert json to csv
+    pdObj = pd.read_json("temp-price.json", orient='index')
+    pdObj.to_csv('ec2-cost.csv', index=False)
+
+
+# Translate region code to region name. Even though the API data contains
+# regionCode field, it will not return accurate data. However using the location
+# field will, but then we need to translate the region code into a region name.
+# You could skip this by using the region names in your code directly, but most
+# other APIs are using the region code.
+def get_region_name(region_code):
+    default_region = 'US East (N. Virginia)'
+    endpoint_file = resource_filename('botocore', 'data/endpoints.json')
+    try:
+        with open(endpoint_file, 'r') as f:
+            data = json.load(f)
+        # Botocore is using Europe while Pricing API using EU...sigh...
+        return data['partitions'][0]['regions'][region_code]['description'].replace('Europe', 'EU')
+    except IOError:
+        return default_region
+
+
+# Use AWS Pricing API through Boto3
+# API only has us-east-1 and ap-south-1 as valid endpoints.
+# It doesn't have any impact on your selected region for your instance.
+client = boto3.client('pricing', region_name='us-east-1')
+
+# Get current price for a given instance, region and os
+price = get_price(get_region_name('ap-southeast-1'))
+print(price)
